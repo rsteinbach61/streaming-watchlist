aeca472ed2c780aa2efe09e2003feb62dd7987db
Richard Steinbach <steinbach.dick@gmail.com>
2018-04-12 13:29:26 -0700

    add #create code to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  #binding.pry
+    if  params[:user][:password] == params[:user][:password_confirmation]
+      user = User.create(user_params)
+      session[:user_id] = user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  def create
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
