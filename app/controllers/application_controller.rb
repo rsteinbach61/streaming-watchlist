f5f02e400bb9b288eadb28d266a09e337e75e8dd
Richard Steinbach <steinbach.dick@gmail.com>
2018-04-17 12:00:23 -0700

    add #current_user and #logged_in? to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @user = (User.find_by(id: session[:user_id]) || User.new)
+  end
+
+  def logged_in?
+    current_user.id != nil
+  end
 end
