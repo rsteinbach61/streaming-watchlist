d70fd110e14921a59d6ae87e9ae493aff9344b04
Richard Steinbach <steinbach.dick@gmail.com>
2018-04-23 13:59:23 -0700

    add uniqueness validation for User's name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :shows, through: :watchlists
   has_many :watchlists
   has_secure_password
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, email: true
 
 end
