371c403a41e42ba59ad34d2a57d4e4cc7a83b859
Richard Steinbach <steinbach.dick@gmail.com>
2018-05-03 13:47:56 -0700

    Add validation and error handling for new show when no watchlist is chosen, and add link to create new watchlist as part of the error message.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ApplicationRecord
   belongs_to :watchlist, optional: true
   has_many :comments, dependent: :delete_all
-  validates :show_title, :show_type, presence: true
+  validates :show_title, :show_type, :watchlist_id, presence: true
 
   scope :genre, ->(genre) {where("genre = ?", genre)}
   scope :show_type, ->(show_type) {where("show_type = ?", show_type)}
