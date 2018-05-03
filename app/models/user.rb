551188309c48ecadeedb9bc9d0714785edac9fae
Richard Steinbach <steinbach.dick@gmail.com>
2018-05-03 11:40:49 -0700

    Add a HMT association user->show-comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-
-  has_many :shows, through: :watchlists
   has_many :watchlists
+  has_many :shows, through: :watchlists
+  has_many :comments, through: :shows
+
   has_secure_password
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, email: true
