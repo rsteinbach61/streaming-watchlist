5091f8e5e4549229b73f0e58f79dc1522506607c
Richard Steinbach <steinbach.dick@gmail.com>
2018-05-07 11:45:21 -0700

    Add second helper for error messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,16 @@
 module ApplicationHelper
 
   def error_helper(object)
+      content_tag(:h2, "Oops:", id: "error_explanation")
 
-      content_tag(:div, content_tag(:h2, "Failed"), id: "error_explanation")
+  end
 
+  def error_list(object)
       content_tag(:ul, :class => "error msgs") do
         object.errors.full_messages.each do |msg|
         concat content_tag(:li, msg)
         end
       end
+
   end
 end
