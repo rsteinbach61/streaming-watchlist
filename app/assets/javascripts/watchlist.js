716b2a07457692302ec8e7c614bd887e0a069df8
Richard Steinbach <steinbach.dick@gmail.com>
2018-09-14 09:54:34 -0700

    add display comments funtion in watchlist.js, add data-show to Get Comments button on Shows show pages.

--- app/assets/javascripts/watchlist.js
+++ app/assets/javascripts/watchlist.js
@@ -8,16 +8,19 @@ function attachListeners(){
   $('#search').on('click', function(){
     alert("Search");
   });
+
+  $('#get_comments').on('click', displayComments);
 }
 
 function displayComments(){
+  show = document.querySelector("#get_comments")
   //$("#games").empty();
-  $.get('/comments', function(comments){
+  $.get(`/shows/${show.dataset.show}/comments`, function(comments){
     comments.data.forEach(function(c){
-      $("#comments").append(`<button onclick="getComment(${c.id})">${c.title}</button><br>`);
+      $("#comments").append(`<button onclick="getComment(${c.id})">${c.attributes.title}</button><br>`);
 })})
 }
 
-function getComment(){
-  
+function getComment(id){
+  alert("get comment")
 }
