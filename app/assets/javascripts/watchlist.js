7b69cc8e37d9d9d5e52cd6166a7b5812e44a6495
Richard Steinbach <steinbach.dick@gmail.com>
2018-09-14 09:59:05 -0700

    change comments from buttons to paras

--- app/assets/javascripts/watchlist.js
+++ app/assets/javascripts/watchlist.js
@@ -14,10 +14,9 @@ function attachListeners(){
 
 function displayComments(){
   show = document.querySelector("#get_comments")
-  //$("#games").empty();
   $.get(`/shows/${show.dataset.show}/comments`, function(comments){
     comments.data.forEach(function(c){
-      $("#comments").append(`<button onclick="getComment(${c.id})">${c.attributes.title}</button><br>`);
+      $("#comments").append(`<p onclick="getComment(${c.id})">${c.attributes.title}</p>`);
 })})
 }
 
