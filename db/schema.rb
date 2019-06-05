ec31e25e2ba9b933110083d3b7a95db7426c3fc7
Richard Steinbach <steinbach.dick@gmail.com>
2019-06-05 08:33:34 -0700

    change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180927231405) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "body"
